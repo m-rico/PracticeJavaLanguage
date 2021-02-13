deae69f7eb96dd6778b4da0b5921c6e13777b83c
m-rico <adhilacahya20>
2021-02-13 23:19:59 +0700

    update kerjaan diserang banten

--- src/com/driko/AdvanceString.java
+++ src/com/driko/AdvanceString.java
@@ -1,8 +1,10 @@
 package com.driko;
 
+import java.time.Year;
+
 public class AdvanceString {
     public static void main(String[] args) throws Exception {
-        System.out.println("\n Kim Dahyun \n\n");
+        System.out.println("Kim Dahyun \n\n");
 
         // Todo Pengenalan String
         /*
@@ -12,25 +14,55 @@ public class AdvanceString {
                 The String object is used to represent and manipulate a sequence of characters.
         */
 
+        //create String variable
         String kimString = "Dahyun";
 
         // create array char
 
         char[] arrayKimString = {'D', 'a', 'h', 'y', 'u', 'n'};
+        System.out.print("Mencetak arrayKimString : ");
+        System.out.println(arrayKimString);
         int i = 0;
         int m = arrayKimString.length;
 
+        // melakukan print atau cetak terhadap array kim
+        System.out.println("Dengan String : " + kimString); // menampilkan String array
+        System.out.print("Dengan LOOP array char : ");
 
-        System.out.println("Dengan String : " + kimString);
-        System.out.print("Dengan array char : ");
+        //membentuk string menggunakan arrayString dengan metode For loop
         for (int k = 0; k < arrayKimString.length; k++){
             System.out.print(arrayKimString[k]);
         }
         System.out.print("\n");
-        arrayKimString[0] = '\'';
-        System.out.println("melakukan sort array char dengan index --> " + arrayKimString[0]);
+
+
+        //merubah huruf pada arrayString
+        System.out.println();
+        arrayKimString[1] = '\'';
+        System.out.print("Melakukan Replace pada arrayKimString : ");
+        System.out.println(arrayKimString);
+
+
+        // merubah string secara tidak langsung, karna string dijava bersifat inmutetable atau tidak bisa dirubah
+        String  chose = kimString.charAt(0) + "\'" + kimString.substring(2,6);
+
+
+        System.out.println("Melakukan Replace pada kimString : " + chose);
 
         System.out.println("\n");
-        arrayKimString.c                               
+        //memecah String array menggunakan Loop, berfungsi menyisipkan spasi diantara char pada array
+        System.out.print("Memecah String menggunakan LOOP :");
+        for (int r = 0; r <kimString.length(); r++){
+            System.out.print(" ");
+            System.out.print(kimString.charAt(r));
+        }
+        System.out.println("\n");
+
+
+        int address = System.identityHashCode(arrayKimString);
+        System.out.println("Mengubah arrayKimString menjadi hashcode : " + address);
+
+        System.out.println("mengubah Hashcode arrayKimString menjadi Hex : " + Integer.toHexString(address));
+
     }// end main method
 }
